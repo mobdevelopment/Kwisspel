b67aa5c5bb56d13ae34b790803e4d730eb2fd7b5
Mark van Kleef <mkleef@avans.nl>
2016-09-21 20:08:07 +0200

    Theme preperation

--- QuizGame/KwisspelRenewed/Model/DummyQuestionRepository.cs
+++ QuizGame/KwisspelRenewed/Model/DummyQuestionRepository.cs
@@ -6,63 +6,38 @@ using System.Threading.Tasks;
 
 namespace KwisspelRenewed.Model
 {
-    class DummyQuestionRepository : IQuestionRepository
+    class DummyQuestionRepository : IThemeRepository
     {
-        public List<Question> ToList()
+        public List<QuizTheme> ToList()
         {
-            var questions = new List<Question>();
+            var themes = new List<QuizTheme>();
 
-            questions.Add(new Question
-            {
-                Id = 1,
-                Text = "Wat is 1 + 1",
-                Category = "Rekenen",
-                Answers = new List<Answer> {
-                new Answer { Id = 1, Text = "2", IsCorrect = true},
-                new Answer { Id = 2, Text = "11", IsCorrect = false}
-            }
+            themes.Add(new QuizTheme {
+                ID = QuizTheme.GENERAL,
+                Name = "Algemeen",
             });
 
-            questions.Add(new Question
-            {
-                Id = 2,
-                Text = "Wat is 2 + 2",
-                Category = "Rekenen",
-                Answers = new List<Answer> {
-                new Answer { Id = 1, Text = "6", IsCorrect = false},
-                new Answer { Id = 2, Text = "8", IsCorrect = false},
-                new Answer { Id = 3, Text = "4", IsCorrect = true},
-                new Answer { Id = 4, Text = "Een rekensom", IsCorrect = true},
-            }
+            themes.Add(new QuizTheme {
+                ID = QuizTheme.SCHOOL,
+                Name = "Educatie",
             });
 
-            questions.Add(new Question
-            {
-                Id = 3,
-                Text = "Wat is liefde",
-                Category = "Rekenen",
-                Answers = new List<Answer> {
-                new Answer { Id = 1, Text = "Baby don't hurt me", IsCorrect = false },
-                new Answer { Id = 2, Text = "An emotion", IsCorrect = true },
-                new Answer { Id = 3, Text = "An hormone", IsCorrect = false }
-
-            }
+            themes.Add(new QuizTheme {
+                ID = QuizTheme.MEDIA,
+                Name = "Media",
             });
 
-            questions.Add(new Question
-            {
-                Id = 4,
-                Text = "Welke programmeertaal is cewl?",
-                Category = "Rekenen",
-                Answers = new List<Answer> {
-                new Answer { Id = 1, Text = "Java", IsCorrect = false },
-                new Answer { Id = 2, Text = "C#", IsCorrect = true },
-                new Answer { Id = 3, Text = "Python", IsCorrect = false},
-                new Answer { Id = 4, Text = "Javascript", IsCorrect = false},
-            }
+            themes.Add(new QuizTheme {
+                ID = QuizTheme.NATURE,
+                Name = "Natuur",
             });
 
-            return questions;
+            themes.Add(new QuizTheme {
+                ID = QuizTheme.POLITICS,
+                Name = "Politiek",
+            });
+
+            return themes;
         }
     }
 }
